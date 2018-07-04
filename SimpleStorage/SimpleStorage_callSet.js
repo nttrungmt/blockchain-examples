15ae9825890ef85642ea65882cb4ebeafabe7b49
Trung Nguyen Thanh <nttrungmt@gmail.com>
2018-07-04 12:14:34 -0500

    123

--- SimpleStorage/SimpleStorage_callSet.js
+++ SimpleStorage/SimpleStorage_callSet.js
@@ -1,4 +1,4 @@
 var simplestorageContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]);
 personal.unlockAccount(eth.coinbase,"test");
-var simplestorageInst = simplestorageContract.at('0x2b806250aff623e10eae6c4b8144b84aac7f24af');
+var simplestorageInst = simplestorageContract.at('0x1e02b7368b3a4e91c682cfde55280925b78ce1d0');
 simplestorageInst.set(5,{from: eth.accounts[0], gas: 3000000})
