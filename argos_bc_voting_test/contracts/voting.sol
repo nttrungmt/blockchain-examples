481e8dbd0991b7815b564d15a0c62f0b175894dc
Trung Nguyen Thanh <nttrungmt@gmail.com>
2018-07-05 13:09:03 -0500

    123

--- argos_bc_voting_test/contracts/voting.sol
+++ argos_bc_voting_test/contracts/voting.sol
@@ -1,84 +1,47 @@
 pragma solidity ^0.4.18;
-// written for Solidity version 0.4.18 and above that doesnt break functionality
+// We have to specify what version of compiler this code will compile with
 
 contract Voting {
-    // an event that is called whenever a Candidate is added so the frontend could
-    // appropriately display the candidate with the right element id (it is used
-    // to vote for the candidate, since it is one of arguments for the function "vote")
-    event AddedCandidate(uint candidateID);
+  /* mapping field below is equivalent to an associative array or hash.
+  The key of the mapping is candidate name stored as type bytes32 and value is
+  an unsigned integer to store the vote count
+  */
+  
+  mapping (bytes32 => uint8) public votesReceived;
+  
+  /* Solidity doesn't let you pass in an array of strings in the constructor (yet).
+  We will use an array of bytes32 instead to store the list of candidates
+  */
+  
+  bytes32[] public candidateList;
 
-    // describes a Voter, which has an id and the ID of the candidate they voted for
-    struct Voter {
-        bytes32 uid; // bytes32 type are basically strings
-        uint candidateIDVote;
-    }
-    // describes a Candidate
-    struct Candidate {
-        bytes32 name;
-        bytes32 party; 
-        // "bool doesExist" is to check if this Struct exists
-        // This is so we can keep track of the candidates 
-        bool doesExist; 
-    }
+  /* This is the constructor which will be called once when you
+  deploy the contract to the blockchain. When we deploy the contract,
+  we will pass an array of candidates who will be contesting in the election
+  */
+  function Voting(bytes32[] candidateNames) public {
+    candidateList = candidateNames;
+  }
 
-    // These state variables are used keep track of the number of Candidates/Voters 
-    // and used to as a way to index them     
-    uint numCandidates; // declares a state variable - number Of Candidates
-    uint numVoters;
+  // This function returns the total votes a candidate has received so far
+  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+    require(validCandidate(candidate));
+    return votesReceived[candidate];
+  }
 
-    // Think of these as a hash table, with the key as a uint and value of 
-    // the struct Candidate/Voter. These mappings will be used in the majority
-    // of our transactions/calls
-    // These mappings will hold all the candidates and Voters respectively
-    mapping (uint => Candidate) candidates;
-    mapping (uint => Voter) voters;
-    
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-     *  These functions perform transactions, editing the mappings *
-     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+  // This function increments the vote count for the specified candidate. This
+  // is equivalent to casting a vote
+  function voteForCandidate(bytes32 candidate) public {
+    require(validCandidate(candidate));
+    votesReceived[candidate] += 1;
+  }
 
-    function addCandidate(bytes32 name, bytes32 party) public {
-        // candidateID is the return variable
-        uint candidateID = numCandidates++;
-        // Create new Candidate Struct with name and saves it to storage.
-        candidates[candidateID] = Candidate(name,party,true);
-        AddedCandidate(candidateID);
+  function validCandidate(bytes32 candidate) view public returns (bool) {
+    for(uint i = 0; i < candidateList.length; i++) {
+      if (candidateList[i] == candidate) {
+        return true;
+      }
     }
-
-    function vote(bytes32 uid, uint candidateID) public {
-        // checks if the struct exists for that candidate
-        if (candidates[candidateID].doesExist == true) {
-            uint voterID = numVoters++; //voterID is the return variable
-            voters[voterID] = Voter(uid,candidateID);
-        }
-    }
-
-    /* * * * * * * * * * * * * * * * * * * * * * * * * * 
-     *  Getter Functions, marked by the key word "view" *
-     * * * * * * * * * * * * * * * * * * * * * * * * * */
-    // finds the total amount of votes for a specific candidate by looping
-    // through voters 
-    function totalVotes(uint candidateID) view public returns (uint) {
-        uint numOfVotes = 0; // we will return this
-        for (uint i = 0; i < numVoters; i++) {
-            // if the voter votes for this specific candidate, we increment the number
-            if (voters[i].candidateIDVote == candidateID) {
-                numOfVotes++;
-            }
-        }
-        return numOfVotes; 
-    }
-
-    function getNumOfCandidates() public view returns(uint) {
-        return numCandidates;
-    }
-
-    function getNumOfVoters() public view returns(uint) {
-        return numVoters;
-    }
-	
-    // returns candidate information, including its ID, name, and party
-    function getCandidate(uint candidateID) public view returns (uint,bytes32, bytes32) {
-        return (candidateID,candidates[candidateID].name,candidates[candidateID].party);
-    }
+    return false;
+  }
 }
